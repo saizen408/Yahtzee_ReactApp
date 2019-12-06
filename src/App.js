a51519c07f79ceeb9d462f0aab2189051c01f7fa
Eric Owusu <llkoole7@gmail.com>
2019-12-06 01:46:45 -0800

    add GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from "react";
-import Game from "./Game";
-import "./App.css";
+import React, { Component } from 'react';
+import { HashRouter } from 'react-router-dom';
+
+import Game from './Game';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Game />
-      </div>
+      <HashRouter basename="/">
+        <div>
+          <div className="App">
+            <Game />
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
